a188d544d353a5bad47630d37e9574bb1c8d7c81
yktimes <1162073988@qq.com>
2019-02-01 21:47:05 +0800

    实现了 判断账号和手机号码 是否存在

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -24,7 +24,8 @@ var vm = new Vue({
         allow: false
     },
     methods: {
-        check_username: function (){
+      // 检查用户名
+    check_username: function (){
             var len = this.username.length;
             if(len<5||len>20) {
                 this.error_name_message = '请输入5-20个字符的用户名';
@@ -32,7 +33,23 @@ var vm = new Vue({
             } else {
                 this.error_name = false;
             }
-
+            // 检查重名
+            if (this.error_name == false) {
+                axios.get(this.host + '/usernames/' + this.username + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_name_message = '用户名已存在';
+                            this.error_name = true;
+                        } else {
+                            this.error_name = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
+            }
         },
         check_pwd: function (){
             var len = this.password.length;
@@ -49,7 +66,8 @@ var vm = new Vue({
                 this.error_check_password = false;
             }
         },
-        check_phone: function (){
+           // 检查手机号
+    check_phone: function (){
             var re = /^1[345789]\d{9}$/;
             if(re.test(this.mobile)) {
                 this.error_phone = false;
@@ -57,7 +75,22 @@ var vm = new Vue({
                 this.error_phone_message = '您输入的手机号格式不正确';
                 this.error_phone = true;
             }
-
+            if (this.error_phone == false) {
+                axios.get(this.host + '/mobiles/'+ this.mobile + '/count/', {
+                        responseType: 'json'
+                    })
+                    .then(response => {
+                        if (response.data.count > 0) {
+                            this.error_phone_message = '手机号已存在';
+                            this.error_phone = true;
+                        } else {
+                            this.error_phone = false;
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error.response.data);
+                    })
+            }
         },
         check_sms_code: function(){
             if(!this.sms_code){
---
+++ yk_mall/yk_mall/apps/users/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+
+from users import views
+from rest_framework.routers import DefaultRouter
+
+
+
+
+
+urlpatterns = [
+    url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
+    url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
+
+
+
+]
+
+
