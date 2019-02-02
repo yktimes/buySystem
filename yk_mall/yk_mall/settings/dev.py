66f7c6e11889cfe4fbb8a6d162cefa4c991a022c
yktimes <1162073988@qq.com>
2019-02-02 11:35:17 +0800

    基于JWT提供了登录,增加支持用户名与手机号均可作为登录账号(重写authenticate)

--- yk_mall/yk_mall/settings/dev.py
+++ yk_mall/yk_mall/settings/dev.py
@@ -229,8 +229,14 @@ REST_FRAMEWORK = {
 import datetime
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'users.utils.jwt_response_payload_handler',
 }
 
 
 # 自定义模型类
 AUTH_USER_MODEL = 'users.User'
+
+# Django使用我们自定义的认证后端
+AUTHENTICATION_BACKENDS = [
+    'users.utils.UsernameMobileAuthBackend',
+]
