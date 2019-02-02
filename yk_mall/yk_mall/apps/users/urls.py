e456b2eed99c0c857103d63bc5fc03d17f8203b2
yktimes <1162073988@qq.com>
2019-02-02 10:32:52 +0800

    实现了　注册功能

--- yk_mall/yk_mall/apps/users/urls.py
+++ yk_mall/yk_mall/apps/users/urls.py
@@ -10,7 +10,7 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
-
+    url(r'^users/$', views.UserView.as_view()),
 
 
 ]
