a352485db890a5f70a8f58303ea072af1ece9fb0
yktimes <1162073988@qq.com>
2019-02-04 11:52:29 +0800

    增加了　收货地址中　省市区地址查询(使用drf-extensions　缓存)

---
+++ yk_mall/yk_mall/apps/areas/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from .models import Area
+
+
+class AreaSerializer(serializers.ModelSerializer):
+    """
+    行政区划信息序列化器
+    """
+
+    class Meta:
+        model =Area
+        fields =('id','name')
+
+
+class SubAreaSerializer(serializers.ModelSerializer):
+    """
+    子行政区划信息序列化器
+    """
+    subs = AreaSerializer(many=True,read_only=True)
+
+    class Meta:
+        model = Area
+        fields = ('id', 'name','subs')
